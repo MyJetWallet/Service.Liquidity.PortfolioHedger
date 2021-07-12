58c7fe7bc2dd4000541aee0075d838c5d3b460fb
geomatika46 <alexey.p@smplt.net>
2021-07-12 13:02:26 +0300

    add logging

--- src/Service.Liquidity.PortfolioHedger/Services/ExternalExchangeTradeService.cs
+++ src/Service.Liquidity.PortfolioHedger/Services/ExternalExchangeTradeService.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using MyJetWallet.Domain.ExternalMarketApi;
 using MyJetWallet.Domain.ExternalMarketApi.Dto;
 using MyJetWallet.Domain.ExternalMarketApi.Models;
 using MyJetWallet.Domain.Orders;
+using Newtonsoft.Json;
 using Service.Liquidity.PortfolioHedger.Grpc;
 using Service.Liquidity.PortfolioHedger.Grpc.Models;
 
@@ -14,26 +16,36 @@ namespace Service.Liquidity.PortfolioHedger.Services
         private readonly IExternalExchangeManager _externalExchangeManager;
         private readonly IExternalMarket _externalMarket;
         private readonly ExchangeTradeWriter _exchangeTradeWriter;
+        private readonly ILogger<ExternalExchangeTradeService> _logger;
 
         public ExternalExchangeTradeService(IExternalExchangeManager externalExchangeManager,
-            IExternalMarket externalMarket, ExchangeTradeWriter exchangeTradeWriter)
+            IExternalMarket externalMarket, ExchangeTradeWriter exchangeTradeWriter,
+            ILogger<ExternalExchangeTradeService> logger)
         {
             _externalExchangeManager = externalExchangeManager;
             _externalMarket = externalMarket;
             _exchangeTradeWriter = exchangeTradeWriter;
+            _logger = logger;
         }
 
         public async Task<CreateManualTradeResponse> CreateManualTradeAsync(CreateManualTradeRequest request)
         {
+            _logger.LogInformation("CreateManualTradeAsync receive message: {requestJson}", JsonConvert.SerializeObject(request));
             try
             {
                 var tradeForExchange = GetTradeForExchange(request);
+                _logger.LogInformation("tradeForExchange: {tradeForExchangeJson}", JsonConvert.SerializeObject(tradeForExchange));
                 
                 var trade = await _externalMarket.MarketTrade(tradeForExchange);
+                
+                _logger.LogInformation("Trade from externalMarket: {tradeJson}", JsonConvert.SerializeObject(trade));
+                
                 await _exchangeTradeWriter.PublishTrade(trade);
             }
             catch (Exception exception)
             {
+                _logger.LogError("CreateManualTradeAsync receive exception: {exceptionJson}", JsonConvert.SerializeObject(exception));
+                
                 return new CreateManualTradeResponse()
                 {
                     Success = false,
