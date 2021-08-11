7e150f30419bb735223de1a65eeb82e2f595bfad
geomatika46 <alexey.p@smplt.net>
2021-08-11 15:54:43 +0300

    create new tests

--- test/Service.Liquidity.PortfolioHedger.Tests/SimulationTester.cs
+++ test/Service.Liquidity.PortfolioHedger.Tests/SimulationTester.cs
@@ -74,5 +74,47 @@ namespace Service.Liquidity.PortfolioHedger.Tests
             Assert.AreEqual(99.9999999999m, balance1.NetUsdVolume);
             Assert.AreEqual(0, balance2.NetVolume);
         }
+        
+        [Test]
+        public async Task Test2()
+        {
+            SetExchanges(new List<string>(){"Binance", "FTX"});
+
+            SetMarketInfos(new Dictionary<string, List<ExchangeMarketInfo>>()
+            {{"Binance", new List<ExchangeMarketInfo>()
+                {TesterBase.ExternalMarket1.MarketInfo,
+                    TesterBase.ExternalMarket2.MarketInfo}},
+                {"FTX", new List<ExchangeMarketInfo>()
+                {TesterBase.ExternalMarket1.MarketInfo,
+                    TesterBase.ExternalMarket2.MarketInfo}}});
+            
+            SetBalance("Binance", "BTC", 5);
+            SetBalance("Binance", "USD", 1000000);
+            SetBalance("FTX", "BTC", 5);
+            SetBalance("FTX", "USD", 1000000);
+            
+            SetPrices("Binance", "BTC", "USD", 30000);
+            SetPrices("Binance", "ETH", "USD", 3000);
+            SetPrices("FTX", "BTC", "USD", 30000);
+            SetPrices("FTX", "ETH", "USD", 3000);
+
+            SetIndexPrice("BTC", 30000);
+            SetIndexPrice("USD", 1);
+            
+            SetPortfolio("BTC", 1);
+            SetPortfolio("USD", -30100);
+            
+            // Hedge +1 BTC ExecuteAutoConvert: FROM BTC, FROMVALUE -1
+            var newPortfolio = await _hedgePortfolioService.ExecuteAutoConvert(new ExecuteAutoConvertRequest()
+            {
+                PortfolioSnapshot = GetPortfolioSnapshot()
+            });
+
+            var balance1 = newPortfolio.BalanceByAsset.First(e => e.Asset == "BTC");
+            var balance2 = newPortfolio.BalanceByAsset.First(e => e.Asset == "USD");
+
+            Assert.AreEqual(-99.9999999999m, balance1.NetUsdVolume);
+            Assert.AreEqual(0, balance2.NetVolume);
+        }
     }
 }
